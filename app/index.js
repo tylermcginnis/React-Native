2fdeddfa2bf5287e74c1c4bf370f196c611ea7d7
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-08-28 21:57:00 -0600

    RemoteReduxDevtools

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { AppContainer } from '~/containers'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import * as reducers from './redux'
+import devTools from 'remote-redux-devtools'
 
 const store = createStore(
   combineReducers(reducers),
-  applyMiddleware(thunk)
+  compose(
+    applyMiddleware(thunk),
+    devTools()
+  )
 )
 
 export default function ReactModoro (props) {
