49fcbdd2f537d8324a29b1e997a5fb347b801119
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-08-30 02:05:08 -0600

    Changes for SettingsUI

--- app/components/Settings/Settings.js
+++ app/components/Settings/Settings.js
@@ -1,10 +1,18 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { ReactModoroNavbar, Close } from '~/components'
-import { colors } from '~/styles'
+import { colors, fontSizes } from '~/styles'
+import Slider from 'react-native-slider'
 
 Settings.propTypes = {
+  timerDuration: PropTypes.number.isRequired,
+  restDuration: PropTypes.number.isRequired,
   onBack: PropTypes.func.isRequired,
+  onTimerChange: PropTypes.func.isRequired,
+  onRestChange: PropTypes.func.isRequired,
+  onRestComplete: PropTypes.func.isRequired,
+  onTimerComplete: PropTypes.func.isRequired,
+  onLogout: PropTypes.func.isRequired,
 }
 
 export default function Settings (props) {
@@ -13,9 +21,37 @@ export default function Settings (props) {
       <ReactModoroNavbar
         title='Settings'
         leftButton={<Close onPress={props.onBack}/>} />
-      <Text>
-        Settings
-      </Text>
+      <View style={styles.sliderContainer}>
+        <Text style={styles.titleText}>Timer Duration</Text>
+        <Text style={styles.valueText}>{props.timerDuration}</Text>
+        <Text style={styles.minutes}>{props.timerDuration === 1 ? 'Minute' : 'Minutes'}</Text>
+        <Slider
+          minimumValue={1}
+          maximumValue={60}
+          onSlidingComplete={props.onTimerComplete}
+          thumbTintColor={colors.border}
+          step={1}
+          minimumTrackTintColor={colors.blue}
+          value={props.timerDuration}
+          onValueChange={props.onTimerChange} />
+      </View>
+      <View style={styles.sliderContainer}>
+        <Text style={styles.titleText}>Rest Duration</Text>
+        <Text style={styles.valueText}>{props.restDuration}</Text>
+        <Text style={styles.minutes}>{props.restDuration === 1 ? 'Minute' : 'Minutes'}</Text>
+        <Slider
+          minimumValue={1}
+          maximumValue={60}
+          onSlidingComplete={props.onRestComplete}
+          thumbTintColor={colors.border}
+          step={1}
+          minimumTrackTintColor={colors.blue}
+          value={props.restDuration}
+          onValueChange={props.onRestChange} />
+      </View>
+      <TouchableOpacity onPress={props.onLogout} style={styles.logout}>
+        <Text style={styles.logoutText}>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -24,5 +60,39 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
-  }
+  },
+  sliderContainer: {
+    flex: 1,
+    marginLeft: 20,
+    marginRight: 20,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 20,
+    color: colors.primary,
+    textAlign: 'center',
+  },
+  valueText: {
+    fontSize: 50,
+    color: colors.blue,
+    textAlign: 'center',
+    padding: 15,
+  },
+  minutes: {
+    color: colors.secondary,
+    textAlign: 'center',
+  },
+  logout: {
+    backgroundColor: colors.blue,
+    alignItems: 'stretch',
+    borderRadius: 25,
+    margin: 25,
+    padding: 10,
+  },
+  logoutText: {
+    color: colors.white,
+    fontSize: fontSizes.secondary,
+    textAlign: 'center',
+  },
 })
--- app/containers/Settings/SettingsContainer.js
+++ app/containers/Settings/SettingsContainer.js
@@ -6,9 +6,36 @@ export default class SettingsContainer extends Component {
   static propTypes = {
     navigator: PropTypes.object.isRequired,
   }
+  state = {
+    timerDuration: 20,
+    restDuration: 5,
+  }
+  handleTimerChange = (timerDuration) => {
+    this.setState({timerDuration})
+  }
+  handleRestChange = (restDuration) => {
+    this.setState({restDuration})
+  }
+  handleTimerComplete = () => {
+    console.log('Finished Sliding Timer')
+  }
+  handleRestComplete = () => {
+    console.log('Finished Sliding Rest Timer')
+  }
+  handleLogout = () => {
+    console.log('Logging Out!')
+  }
   render () {
     return (
-      <Settings onBack={this.props.navigator.pop} />
+      <Settings
+        onBack={this.props.navigator.pop}
+        onLogout={this.handleLogout}
+        onRestComplete={this.handleRestComplete}
+        onTimerComplete={this.handleTimerComplete}
+        onTimerChange={this.handleTimerChange}
+        onRestChange={this.handleRestChange}
+        timerDuration={this.state.timerDuration}
+        restDuration={this.state.restDuration} />
     )
   }
 }
