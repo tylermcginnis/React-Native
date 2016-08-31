dbd6da28208d3518b0aec5ed0643f316037a0123
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-08-30 21:13:07 -0600

    Files for FlashNotification

--- app/components/FlashNotification/FlashNotification.js
+++ app/components/FlashNotification/FlashNotification.js
@@ -1,11 +1,11 @@
 import React, { PropTypes, Component } from 'react'
-import { View, Text, Dimensions, StyleSheet, Animated } from 'react-native'
+import { View, Text, Animated, Dimensions, StyleSheet } from 'react-native'
 const { width } = Dimensions.get('window')
 const NOTIFICATION_WIDTH = width * 0.7
 
 export default class FlashNotification extends Component {
   static propTypes = {
-    length: PropTypes.number,
+    length: PropTypes.number.isRequired,
     permanent: PropTypes.bool.isRequired,
     location: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
@@ -18,13 +18,11 @@ export default class FlashNotification extends Component {
   }
   state = {
     width: new Animated.Value(50),
-    left: new Animated.Value(width * 0.4),
     opacity: new Animated.Value(0.7),
     textOpacity: new Animated.Value(0),
   }
   componentDidMount () {
-    Animated.spring(this.state.width, {toValue: NOTIFICATION_WIDTH, friction: 9}).start(),
-    Animated.spring(this.state.left, {toValue: (width - NOTIFICATION_WIDTH) / 2, friction: 9}).start()
+    Animated.spring(this.state.width, {toValue: NOTIFICATION_WIDTH}).start()
     Animated.timing(this.state.textOpacity, {toValue: 1, duration: 1000}).start()
 
     if (this.props.permanent === false) {
@@ -37,7 +35,6 @@ export default class FlashNotification extends Component {
   getStyle = () => {
     return {
       width: this.state.width,
-      left: this.state.left,
       opacity: this.state.opacity,
       top: this.props.location === 'top' ? 60 : undefined,
       bottom: this.props.location === 'top' ? undefined : 60,
@@ -63,9 +60,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 10,
+    left: (width - NOTIFICATION_WIDTH) / 2
   },
   text: {
-    textAlign: 'center',
-    color: '#fff',
-  }
+    color: '#fff'
+  },
 })
